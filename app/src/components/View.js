08effecd5e8cabced59053827ddc3521b647149c
cleverlight <cleverlight@users.noreply.github.com>
2020-10-30 15:50:16 +0100

    converted components to React Hooks; derived view with `editor` default

--- app/src/components/View.js
+++ app/src/components/View.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
+import qs from 'qs';
 
-const View = (props) => {
-  const params = useParams();
-  const datasource = params.datasource || 'local';
-  return (
-    <div>
-      <h1>View</h1>
-      <ul>
-        <li>I am a view of { params.path }</li>
-        <li>I am a view from { datasource }</li>
-      </ul>
-    </div>
-  );
-};
+function View() {
+    const params = useParams();
+    const query = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+    // derived with defaults
+    const datasource = params.datasource || 'local';
+    const view = query.view || 'editor';
+    console.log(query);
+    return (
+        <div>
+            <h1>View</h1>
+            <ul>
+                <li>I am a view of {params.path}</li>
+                <li>I am a view from {datasource}</li>
+                <li>I am visualised using the `{view}` view</li>
+            </ul>
+        </div>
+    );
+}
 
 export default View;
