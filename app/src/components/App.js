6216499a4547a0cbc5e7e8be159c014f8cafa87a
cleverlight <cleverlight@users.noreply.github.com>
2020-10-30 14:50:40 +0100

    corrected favicon in build

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <h2>Far out in the uncharted backwaters...</h2>
+                <h2>Far out in the uncharted backwaters....</h2>
                 <Switch>
                     <Route path="/@:datasource/:path+" component={View} />
                     <Route path="/:path+" component={View} />
